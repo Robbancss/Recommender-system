3f12eb0e23dc8d1c2be52d704cffbeaff8248f68
Robbancss <robi.panczel@gmail.com>
2017-03-13 03:11:26 +0100

    onclose()

--- hw_1.py
+++ hw_1.py
@@ -65,6 +65,7 @@ def on_error(ws, error):
     print("Error: " + error)
 
 def on_close(ws):
+    print("### closed ###")
     f = open('data.json', 'a')
     f.write(relationTypes)
     f.write(nodeTypes)  
