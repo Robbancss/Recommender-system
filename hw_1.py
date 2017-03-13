cf444b21a75679d1ef4cfe5bf937d70f2ab2f010
Robbancss <robi.panczel@gmail.com>
2017-03-13 03:07:45 +0100

    dumbthings to data.json

--- hw_1.py
+++ hw_1.py
@@ -2,59 +2,64 @@ import websocket
 import _thread
 import json
 
-nodeType = []
-relationType = []
+nodeTypes = []
+relationTypes = []
 
 def on_message(ws, message):
     print("Message: %s" % message)
     if message == "Author name":
-            print("... Sending: " + message)
-            ws.send("Author name:Pánczél Róbert")
-            print("Sent: " + message)
+        print("... Sending: " + message)
+        ws.send("Author name:Pánczél Róbert")
+        print("Sent: " + message)
             
     elif message == "Author id":
-            print("... Sending: " + message)
-            ws.send("Author id:EY2P3W")
-            print("Sent: " + message)
+        print("... Sending: " + message)
+        ws.send("Author id:EY2P3W")
+        print("Sent: " + message)
             
     elif message == "Author email":
-            print("... Sending: " + message)
-            ws.send("Author email:robi.panczel@gmail.com")
-            print("Sent: " + message)
+        print("... Sending: " + message)
+        ws.send("Author email:robi.panczel@gmail.com")
+        print("Sent: " + message)
             
-    elif message == "Add node type:":
-            nodetype = message[14:].lstrip(' ')
-            print("... Adding node type: " + nodetype)
-            nodeType.append(nodetype)
-            #ws.send("Node type added:" + nodetype)
-            print("Sent: Node type added: " + nodetype)
-            
-    elif message == "Add relation type:":
-            reltype = message[18:].lstrip(' ')
-            print("... Adding relation type: " + reltype)
-            relationType.append(reltype)
-            #ws.send("Relation type added:" + reltype)
-            print("Sent: Relation type added: " + reltype)
-            
+    elif message[:14] == "Add node type:":
+        print("... Sending: " + message)
+        nodetype = message[14:].lstrip(' ')
+        nodeTypes.append(nodetype)
+        ws.send("Node type added:" + nodetype)
+        print("Sent: " + message)
+        f = open('data.json', 'a')
+        f.write(nodetype)  
+
+    elif message[:18] == "Add relation type:":
+        print("... Sending: " + message)
+        relationtype = message[18:].lstrip(' ')
+        relationTypes.append(relationtype)
+        ws.send("Relation type added:" + relationtype)
+        print("Sent: " + message)
+        f = open('data.json', 'a')
+        f.write(relationtype)  
+
     elif message == "Get node types":
-            print("... Sending: " + message)
-            #ws.send("Node types:" + json.dumps(nodeType))
-            print("Sent: ")
-            print(nodeType)
-            
+        print("... Sending: " + message)
+        ws.send("Node types:" + json.dumps(nodeTypes))
+        print("Sent node types: ")
+        print(nodeTypes)
+        
     elif message == "Get relation types":
-            print("... Sending: " + message)
-            #ws.send("Relation types:" + json.dumps(relationType))
-            print("Sent: ")
-            print(relationType)
-            
+        print("... Sending: " + message)
+        ws.send("Relation types:" + json.dumps(relationTypes))
+        print("Sent relation types: ")
+        print(relationTypes)
+        print()
+        
     else:
-            print("... Custom message: " + message)
-            nodeType.append(message)
-            ws.send(json.dumps(nodeType))
-            print("Sent in JSON: ")
-            print(json.dumps(nodeType))
-            ws.close()
+        print("... Custom message: " + message)
+        nodeTypes.append(message)
+        ws.send(json.dumps(nodeTypes))
+        print("Sent in JSON: ")
+        print(json.dumps(nodeTypes))
+        ws.close()
 
 def on_error(ws, error):
     print("Error: " + error)
@@ -62,17 +67,10 @@ def on_error(ws, error):
 def on_close(ws):
     print("### closed ###")
 
-def on_open(ws):
-    def run(*args):
-        ws.send("Handsake")
-    _thread.start_new_thread(run, ())
-
-
 if __name__ == "__main__":
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://echo.websocket.org/",
+    ws = websocket.WebSocketApp("ws://service.recommender-system.com/recsys2-course/engine",
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
-    ws.on_open = on_open
     ws.run_forever()
