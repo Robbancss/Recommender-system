78febc80735ff96bcfcc7624a7ddb5d1ca674d02
Robbancss <robi.panczel@gmail.com>
2017-04-03 01:35:02 +0200

    linesformatted

--- hw_1.py
+++ hw_1.py
@@ -5,23 +5,24 @@ import json
 nodeTypes = []
 relationTypes = []
 
+
 def on_message(ws, message):
     print("Message: %s" % message)
     if message == "Author name":
         print("... Sending: " + message)
         ws.send("Author name:Pánczél Róbert")
         print("Sent: " + message)
-            
+
     elif message == "Author id":
         print("... Sending: " + message)
         ws.send("Author id:EY2P3W")
         print("Sent: " + message)
-            
+
     elif message == "Author email":
         print("... Sending: " + message)
         ws.send("Author email:robi.panczel@gmail.com")
         print("Sent: " + message)
-            
+
     elif message[:14] == "Add node type:":
         print("... Sending: " + message)
         nodetype = message[14:].lstrip(' ')
@@ -29,7 +30,7 @@ def on_message(ws, message):
         ws.send("Node type added:" + nodetype)
         print("Sent: " + message)
         f = open('data.json', 'a')
-        f.write(nodetype)  
+        f.write(nodetype)
 
     elif message[:18] == "Add relation type:":
         print("... Sending: " + message)
@@ -38,21 +39,21 @@ def on_message(ws, message):
         ws.send("Relation type added:" + relationtype)
         print("Sent: " + message)
         f = open('data.json', 'a')
-        f.write(relationtype)  
+        f.write(relationtype)
 
     elif message == "Get node types":
         print("... Sending: " + message)
         ws.send("Node types:" + json.dumps(nodeTypes))
         print("Sent node types: ")
         print(nodeTypes)
-        
+
     elif message == "Get relation types":
         print("... Sending: " + message)
         ws.send("Relation types:" + json.dumps(relationTypes))
         print("Sent relation types: ")
         print(relationTypes)
         print()
-        
+
     else:
         print("... Custom message: " + message)
         nodeTypes.append(message)
@@ -61,19 +62,22 @@ def on_message(ws, message):
         print(json.dumps(nodeTypes))
         ws.close()
 
+
 def on_error(ws, error):
     print("Error: " + error)
 
+
 def on_close(ws):
-    prisnt("### closed ###")
+    print("### closed ###")
     f = open('data.json', 'a')
     f.write(relationTypes)
-    f.write(nodeTypes)  
+    f.write(nodeTypes)
+
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp("ws://service.recommender-system.com/recsys2-course/engine",
-                              on_message = on_message,
-                              on_error = on_error,
-                              on_close = on_close)
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
     ws.run_forever()
