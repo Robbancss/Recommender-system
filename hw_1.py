bdd84255aa58acf7e1f86d57d5025888093e9b3f
Robbancss <robi.panczel@gmail.com>
2017-04-05 15:44:39 +0200

    getnode & getrelation added

--- hw_1.py
+++ hw_1.py
@@ -3,7 +3,10 @@ import _thread
 import websocket
 
 nodeTypes = []
+nodeCount = []
 relationTypes = []
+relationCount = []
+
 
 def on_message(ws, message):
     print("Message: %s" % message)
@@ -26,9 +29,9 @@ def on_message(ws, message):
 
     # my token : b92kylvrdw
     elif message == "Token":
-        print("...Sending ",message, " b92kylvrdw")
+        print("...Sending ", message, " b92kylvrdw")
         ws.send("Token:b92kylvrdw")
-        print("Sent: ",message)
+        print("Sent: ", message)
 
     # adding types
     elif message[:14] == "Add node type:":
@@ -48,7 +51,7 @@ def on_message(ws, message):
         ws.send("Relation type added:" + relation)
         print("Sent: " + message)
         # fajl = open('relation_type.json', 'a')
-        # fajl.write(json.dumps(relation))
+        # fajl.write(json.dumps())
         # fajl.close()
 
     # getting types
@@ -76,6 +79,7 @@ def on_message(ws, message):
         fajl.writelines(json.dumps(newNode))
         fajl.close()
         ws.send("Node added")
+        nodeCount.append('1')
 
     # Add relation
     elif message[:13] == "Add relation:":
@@ -85,6 +89,23 @@ def on_message(ws, message):
         fajl.write(json.dumps(newRelation))
         fajl.close()
         ws.send("Relation added")
+        relationCount.append('2')
+
+    # Get Node Count
+    elif message == "Get node count":
+        print("..Sending Node Count")
+        fajl = open('data.json', 'a')
+        fajl.write(json.dumps(['Node count:', len(nodeCount)]))
+        fajl.close()
+        ws.send("Node count:", nodeCount)
+
+    # Get relation Count
+    elif message == "Get relation count":
+        print("..Sending relation count")
+        fajl = open('data.json', 'a')
+        fajl.write(json.dumps(['Relation count:', len(relationCount)]))
+        fajl.close()
+        ws.send("Relation count:", relationCount)
 
     # unknown message
     else:
@@ -102,16 +123,20 @@ def on_error(ws, error):
 
 def on_close(ws):
     print("### closed ###")
-    # f = open('data.json', 'a')
-    # f.write(relationTypes)
-    # f.write(nodeTypes)
+    fajl = open('data.json', 'a')
+    fajl.write(json.dumps(['Node Count:', nodeCount.count('1')]))
+    fajl.write(json.dumps(['Relation Count:', relationCount.count('2')]))
+    fajl.write(json.dumps([nodeTypes]))
+    fajl.write(json.dumps([relationTypes]))
+    fajl.close()
 
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp("ws://service.recommender-system.com/recsys2-course/engine",
-    # ws = websocket.WebSocketApp("ws://echo.websocket.org",
-    
+                                # ws =
+                                # websocket.WebSocketApp("ws://echo.websocket.org",
+
                                 on_message=on_message,
                                 on_error=on_error,
                                 on_close=on_close)
