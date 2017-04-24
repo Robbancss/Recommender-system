79692f7204a89da249b7e73a8930442cdc3cc781
Robbancss <robi.panczel@gmail.com>
2017-04-24 02:23:29 +0200

    simplifying with functions

--- hw_1.py
+++ hw_1.py
@@ -1,126 +1,84 @@
 import json
-import _thread
 import websocket
-import re
-import pprint
+# import _thread
+# import re
+# import pprint
 
 nodeTypes = []
 relationTypes = []
 nodeCount = 0
 relationCount = 0
 
-def comunicate(serverMessage, answer):
-    print("SERVER:"+ serverMessage)
+def communication(serverMessage, answer):
+    print("SERVER: "+ serverMessage)
     ws.send(answer)
-    print("ME:"+ answer)
+    print("ME: "+ answer)
 
 def on_message(ws, message):
-    print("Message: %s" % message)
+    # print("Message: %s" % message)
 
     global nodeCount
     global relationCount
-    # basic identification
+
     if message == "Author name":
-        print("... Sending: " + message)
-        ws.send("Author name:Pánczél Róbert")
-        print("Sent: " + message)
+        communication(message, "Author name:Pánczél Róbert")
 
     elif message == "Author id":
-        print("... Sending: " + message)
-        ws.send("Author id:EY2P3W")
-        print("Sent: " + message)
+        communication(message, "Author id:EY2P3W")
 
     elif message == "Author email":
-        print("... Sending: " + message)
-        ws.send("Author email:robi.panczel@gmail.com")
-        print("Sent: " + message)
+        communication(message, "Author email:robi.panczel@gmail.com")
 
-    # my token : b92kylvrdw
     elif message == "Token":
-        print("...Sending ", message, " b92kylvrdw")
-        ws.send("Token:b92kylvrdw")
-        print("Sent: ", message)
+        communication(message, "Token:b92kylvrdw")
 
-    # adding types
     elif message[:14] == "Add node type:":
-        print("... Sending: " + message)
         node = message[14:].lstrip(' ')
         nodeTypes.append(node)
-        ws.send("Node type added:" + node)
-        print("Sent: " + message)
-        # fajl = open('node_type.json', 'a')
-        # fajl.write(json.dumps(node))
-        # fajl.close()
+        communication(message, "Node type added:" + node)
 
     elif message[:18] == "Add relation type:":
-        print("... Sending: " + message)
         relation = message[18:].lstrip(' ')
         relationTypes.append(relation)
-        ws.send("Relation type added:" + relation)
-        print("Sent: " + message)
-        # fajl = open('relation_type.json', 'a')
-        # fajl.write(json.dumps())
-        # fajl.close()
+        communication(message, "Relation type added:" + relation)
 
-    # getting types
     elif message == "Get node types":
-        print("... Sending: " + message)
-        ws.send("Node types:" + json.dumps(nodeTypes))
-        print("Sent node types: ", nodeTypes)
+        communication(message, "Node types:" + json.dumps(nodeTypes))
 
     elif message == "Get relation types":
-        print("... Sending: " + message)
-        ws.send("Relation types:" + json.dumps(relationTypes))
-        print("Sent relation types: ", relationTypes)
+        communication(message, "Relation types:" + json.dumps(relationTypes))
 
-    # checking the knowledge base
     elif message[:22] == "Select knowledge base:":
-        print("... Sending knowledge base")
-        ws.send("Knowledge base has been created")
-        print("Sent Knowledge base has been created")
+        communication(message, "Knowledge base has been created")
 
     # elif message[:22] == "Select knowledge base:":
-    #     print("..Sending knowledge base")
-    #     ws.send("Knowledge base has been selected")
-    #     print("Sent Knowledge base has been selected")
+    #     communication(message,"Knowledge base has been selected")
 
-    # Add node
     elif message[:9] == "Add node:":
-        print("..Sending:", message)
         newNode = str(message[9:])
         fajl = open('node_type.json', 'a')
         fajl.writelines(newNode+"\n")
-        ws.send("Node added")
         nodeCount = nodeCount + 1
-        # fajl.close()
+        communication(message, "Node added")
 
-    # Add relation
     elif message[:13] == "Add relation:":
-        print("..Sending:", message)
         newRelation = str(message[13:])
         fajl = open('relation_type.json', 'a')
         fajl.print(newRelation)
-        ws.send("Relation added")
         relationCount = relationCount + 1
-        # fajl.close()
+        communication(message, "Relation added")
 
-    # Get Node Count
     elif message == "Get node count":
-        print("..Sending Node Count")
         fajl = open('data.json', 'a')
         a = str(nodeCount)
-        fajl.write(json.dumps(['Node count:', a]))
-        ws.send("Node count:"+a)
-        # fajl.close()
+        fajl.write(['Node count:', a])
+        communication(message, "Node count:"+a)
 
-    # Get relation Count
     elif message == "Get relation count":
-        print("..Sending relation count")
         fajl = open('data.json', 'a')
         a = str(relationCount)
-        fajl.write(json.dumps(['Relation count:', a]))
-        ws.send("Relation count:"+a)
-        # fajl.close()
+        fajl.write(['Relation count:', a])
+        communication(message, "Relation count:"+a)
 
     # unknown message
     else:
@@ -139,10 +97,10 @@ def on_error(ws, error):
 def on_close(ws):
     print("### closed ###")
     fajl = open('data.json', 'a')
-    fajl.write(json.dumps(['Node Count:', nodeCount]))
-    fajl.write(json.dumps(['Relation Count:', relationCount]))
-    fajl.write(json.dumps([nodeTypes]))
-    fajl.write(json.dumps([relationTypes]))
+    fajl.write(['Node Count:', nodeCount])
+    fajl.write(['Relation Count:', relationCount])
+    fajl.write([nodeTypes])
+    fajl.write([relationTypes])
     fajl.close()
 
 
